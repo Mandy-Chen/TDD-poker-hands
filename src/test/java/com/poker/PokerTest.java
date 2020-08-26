3fa4bc1b0a97168a2fd3fa13c3cf7f80e64428af
MandyChen <497133212@qq.com>
2020-08-26 19:49:28 +0800

    [poker][hovees | mandy] test : add should_return_Black_wins_with_full_house_when_play_poker_given_Black_2H4S4C2D4H_White_2S8SASQS3S

--- src/test/java/com/poker/PokerTest.java
+++ src/test/java/com/poker/PokerTest.java
@@ -1,4 +1,5 @@
 package com.poker;
+
 import com.poker.PlayPoker;
 import org.junit.jupiter.api.Test;
 
@@ -9,16 +10,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PokerTest {
     @Test
-    void should_return_White_wins_with_high_card_when_play_poker_given_Black_2H3D5S9CKD_white_2C3H4S8CAH() {
+    void should_return_White_wins_with_high_card_when_play_poker_given_Black_2H3D5S9CKD_White_2C3H4S8CAH() {
         //given
-        List<String> black= Arrays.asList("2H","3D","5S","9C","KD");
-        List<String> white =Arrays.asList("2C","3H","4S","8C","AH");
+        List<String> black = Arrays.asList("2H", "3D", "5S", "9C", "KD");
+        List<String> white = Arrays.asList("2C", "3H", "4S", "8C", "AH");
 
         //when
-        PlayPoker playPoker=new PlayPoker();
-        String result=playPoker.play(black,white);
+        PlayPoker playPoker = new PlayPoker();
+        String result = playPoker.play(black, white);
 
         //then
         assertThat(result).isEqualTo("White wins. -with high card : Ace");
+    }
+
+    @Test
+    void should_return_Black_wins_with_full_house_when_play_poker_given_Black_2H4S4C2D4H_White_2S8SASQS3S() {
+        //given
+        List<String> black = Arrays.asList("2H", "4S", "4C", "2D", "4H");
+        List<String> white = Arrays.asList("2S", "8S", "AS", "QS", "3S");
+
+        //when
+        PlayPoker playPoker = new PlayPoker();
+        String result = playPoker.play(black, white);
+
+        //then
+        assertThat(result).isEqualTo("Black wins. -with full house : 4 over 2");
     }
 }

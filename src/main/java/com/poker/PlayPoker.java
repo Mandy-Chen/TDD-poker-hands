7986cf80c4023f0c8990c2a3c465c9a6fda61f4a
MandyChen <497133212@qq.com>
2020-08-26 14:35:07 +0800

    [poker][hovees | mandy] test : add should_return_White_wins_with_high_card_when_play_poker_given_Black_2H3D5S9CKD_white_2C3H4S8CAH

---
+++ src/main/java/com/poker/PlayPoker.java
@@ -0,0 +1,10 @@
+package com.poker;
+
+import java.util.*;
+
+public class PlayPoker {
+    public String play(List<String> black, List<String> white) {
+       return "";
+    }
+
+}
---
+++ src/test/java/com/poker/PokerTest.java
@@ -0,0 +1,24 @@
+package com.poker;
+import com.poker.PlayPoker;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PokerTest {
+    @Test
+    void should_return_White_wins_with_high_card_when_play_poker_given_Black_2H3D5S9CKD_white_2C3H4S8CAH() {
+        //given
+        List<String> black= Arrays.asList("2H","3D","5S","9C","KD");
+        List<String> white =Arrays.asList("2C","3H","4S","8C","AH");
+
+        //when
+        PlayPoker playPoker=new PlayPoker();
+        String result=playPoker.play(black,white);
+
+        //then
+        assertThat(result).isEqualTo("White wins. -with high card : Ace");
+    }
+}
